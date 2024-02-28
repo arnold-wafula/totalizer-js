2620ee5f944760ef582d00a99c6223d0d9e28819
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-02-28 12:01:37 +0300

    Created new form for calculator data

---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Totalizer JS</title>
+</head>
+<body>
+    <h2>Landed Cost Calculator</h2>
+    <form action="" method="get">
+        <input type="number" name="" id="">
+    </form>
+</body>
+</html>
