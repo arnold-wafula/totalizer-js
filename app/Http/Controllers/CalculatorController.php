d1d5524266ea820811fc193e9a1f916dda21c288
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-02-29 10:21:08 +0300

    Created a calculate function to be called inside the route file on calculate button pressed

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -9,4 +9,13 @@ class CalculatorController extends Controller
     public function index() {
         return view('calculator');
     }
+
+    public function calculate(Request $request) {
+        $num1 = $request->input('num1');
+        $num2 = $request->input('num2');
+
+        $sum = $num1 + $num2;
+
+        return response()->json(['sum' => $sum]);
+    }
 }
