755b7505646e2c14864c53e4f93e0562d4cb4d94
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-02-29 10:19:33 +0300

    Added csrf token to my form

--- resources/views/calculator.blade.php
+++ resources/views/calculator.blade.php
@@ -8,8 +8,43 @@
 </head>
 <body>
     <h2>Landed Cost Calculator</h2>
-    <form action="" method="get">
-        <input type="number" name="" id="">
+    <form action="" method="post">
+        @csrf
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="num1">Number 1</label>
+                        <input type="number" class="form-control" id="num1" name="num1">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label for="num2">Number 2</label>
+                        <input type="number" class="form-control" id="num2" name="num2">
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <button id="calculate-button">Add</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <p id="answer"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
     </form>
+
+    <script src="{{ asset('js/calculator.js') }}"></script>
 </body>
 </html>
